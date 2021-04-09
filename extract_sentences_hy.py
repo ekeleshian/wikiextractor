e5165486147bb576d2f6326ae50a5c639daadbdb
Elizabeth Keleshian <ekeleshian@gmail.com>
2021-04-09 10:49:36 -0400

    adding western armenian extraction logic

--- extract_sentences_hy.py
+++ extract_sentences_hy.py
@@ -1,8 +1,10 @@
 import json
 import sys
 import pickle
+import random
 import re
 from threading import Thread, Lock
+import pandas as pd
 
 mutex = Lock()
 approved_sentences = []
@@ -39,7 +41,7 @@ def process_article(folders):
     for ch in folders:
         if ch == "_":
             return
-        for i in range(100):
+        for i in range(40):
             if i < 10:
                 i = ONES[str(i)]
             filename = f'text/A{ch}/wiki_{i}'
@@ -72,16 +74,41 @@ def process_article(folders):
 
 if __name__ == '__main__':
 
-    threads = []
-    for let_1, let_2 in [("A", "B"), ("C", "D"), ("E", "F"), ("G", "_")]:
-        threads.append(Thread(target=process_article, args=([let_1, let_2],)))
-    for idx, t in enumerate(threads):
-        print(f"starting thread {idx + 1}\n")
-        t.start()
-    for t in threads:
-        t.join()
+    if len(sys.argv) < 2:
+        print("must pass dialect code as first param: hy for eastern armenian, hyw for western armenian\n")
+
+    if sys.argv[1] == "hy":
+        file_name = 'hy_wiki'
+        sample_size = 4097
+        threads = []
+        for let_1, let_2 in [("A", "B"), ("C", "D"), ("E", "F"), ("G", "_")]:
+            threads.append(Thread(target=process_article, args=([let_1, let_2],)))
+        for idx, t in enumerate(threads):
+            print(f"starting thread {idx + 1}\n")
+            t.start()
+        for t in threads:
+            t.join()
+    else:
+        file_name = 'hyw_wiki'
+        process_article(["A", "_"])
+        sample_size = 3172
 
     print(f'dumping approved_sentences  (len: {len(approved_sentences)})')
-    with open("hy_wiki.txt", "w") as f:
+    with open(f"{file_name}.txt", "w") as f:
         for sentence in approved_sentences:
             f.write(sentence+'\n')
+
+    sample = random.sample(approved_sentences, sample_size)
+
+    df = pd.DataFrame({"Sentence": sample,
+                       "Sentence_Eval_1": list([" "]*len(sample)),
+                       "Sentence_Eval_2": list([" "]*len(sample)),
+                       "Sentence_Eval_3": list([" "]*len(sample)),
+                       "Comments": list([" "]*len(sample))})
+    
+    df.to_csv(f"{file_name}.csv")
+
+
+
+
+
